6b5a2aac07191d386e7d4822b959cbcbdd1c3e5d
Yuvi Masory <ymasory@gmail.com>
2010-12-24 17:27:20 -0500

    use subprojects

--- project/build/ScalaCorpusProject.scala
+++ project/build/ScalaCorpusProject.scala
@@ -1,18 +1,22 @@
 import sbt._
 
-class ScalaCorpusProject(info: ProjectInfo) extends DefaultProject(info) with ScalazBoilerplate with Exec {
+class ScalaCorpusProject(info: ProjectInfo) extends DefaultProject(info) with Exec {
 
   //project name
   override val artifactID = "scala-corpus"
 
-  //scalaz
-  override def mainSourceRoots = super.mainSourceRoots +++ srcManagedScala##
-  override def compileAction = super.compileAction dependsOn(generateTupleW)
-  val servlet = "javax.servlet" % "servlet-api" % "2.5" withSources
-  
   //turn down logging level to 'warn'
   log.setLevel(Level.Warn)
 
   //compiler options
   override def compileOptions = Deprecation :: Unchecked :: ExplainTypes :: Nil
+
+  //projects
+  lazy val scalaz = project("scalaz", "scalaz")
+
+  class ScalazProject(info: ProjectInfo) extends ParentProject(inf) with ScalazBoilerplate {
+    override def mainSourceRoots = super.mainSourceRoots +++ srcManagedScala##
+    override def compileAction = super.compileAction dependsOn(generateTupleW)
+    val servlet = "javax.servlet" % "servlet-api" % "2.5" withSources
+  }
 }
