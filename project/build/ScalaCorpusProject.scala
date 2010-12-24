40eef3a2b109623ed2235b09106b865032496968
Yuvi Masory <ymasory@gmail.com>
2010-12-24 18:32:22 -0500

    use subproj for scalaz

--- project/build/ScalaCorpusProject.scala
+++ project/build/ScalaCorpusProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class ScalaCorpusProject(info: ProjectInfo) extends DefaultProject(info) with Exec {
+class ScalaCorpusProject(info: ProjectInfo) extends DefaultProject(info)  with Exec {
 
   //project name
   override val artifactID = "scala-corpus"
@@ -12,11 +12,11 @@ class ScalaCorpusProject(info: ProjectInfo) extends DefaultProject(info) with Ex
   override def compileOptions = Deprecation :: Unchecked :: ExplainTypes :: Nil
 
   //projects
-  lazy val scalaz = project("scalaz", "scalaz")
+  lazy val scalaz = project("scalaz", "scalaz", new ScalazProject(_))
 
-  class ScalazProject(info: ProjectInfo) extends ParentProject(inf) with ScalazBoilerplate {
+  class ScalazProject(info: ProjectInfo) extends DefaultProject(info) with ScalazBoilerplate {
+    override def libraryDependencies = Set("javax.servlet" % "servlet-api" % "2.5" withSources)
     override def mainSourceRoots = super.mainSourceRoots +++ srcManagedScala##
     override def compileAction = super.compileAction dependsOn(generateTupleW)
-    val servlet = "javax.servlet" % "servlet-api" % "2.5" withSources
   }
 }
