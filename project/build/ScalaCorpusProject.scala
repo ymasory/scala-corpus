57979d2b557a9e5a9f9ba9bf4600cd3bc73d19d2
Yuvi Masory <ymasory@gmail.com>
2010-12-24 20:30:41 -0500

    remove scalaz tests

--- project/build/ScalaCorpusProject.scala
+++ project/build/ScalaCorpusProject.scala
@@ -15,12 +15,13 @@ class ScalaCorpusProject(info: ProjectInfo) extends DefaultProject(info)  with E
 
   //projects
   lazy val scalaz = project("scalaz", "scalaz", new ScalazProject(_))
-  lazy val scalaquery = project("scala-query", "scala-query", new ScalaQueryProject(_))
+  // lazy val scalaquery = project("scala-query", "scala-query", new ScalaQueryProject(_))
 
   class ScalazProject(info: ProjectInfo) extends DefaultProject(info) with ScalazBoilerplate {
     val servlets = "javax.servlet" % "servlet-api" % "2.5" withSources
-    val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test"
-    val specs = "org.scala-tools.testing" % "specs" % "1.6.2.2" % "test"
+    // val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test"
+    // val scalaToolsSnapshots = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
+    // val specs = "org.scala-tools.testing" %% "specs" % "1.6.7-SNAPSHOT" % "test" withSources
     override def mainSourceRoots = super.mainSourceRoots +++ srcManagedScala##
     override def compileAction = super.compileAction dependsOn(generateTupleW)
   }
